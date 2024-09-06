7d0b45cf9a588c74044a90ab3dd31146a7098e75
IntanNurzari <intannurzari27@gmail.com>
2024-09-06 21:52:14 +0700

    Revert "TUGAS PBO1_INTAN NURZARI"

--- pertemuan1_intan.py
+++
@@ -1,8 +0,0 @@
-#TASK 1
-
-nama = input ("masukkan nama kamu: ")
-nama_panggilan = input ("masukkan nama panggilan kamu: ")
-
-print("Nama saya," + nama )
-print("Kamu bisa panggil saya " + nama_panggilan)
-
