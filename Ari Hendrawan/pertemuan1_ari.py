37e14bd57e43da708dd02a7973f2e8fde18ef85f
Envious19 <ari.hendrawan50@gmail.com>
2024-09-20 10:20:46 +0700

    Add files via upload

---
+++ Ari Hendrawan/pertemuan1_ari.py
@@ -0,0 +1,8 @@
+Nama = "Ari Hendrawan"
+Nama_Panggilan = "Ari atau Envy"
+
+# Nama = input("Masukkan Nama Kamu :")
+# Nama_Panggilan = input("Masukkan Nama Panggilan Kamu :")
+
+print("Nama saya, "+ Nama)
+print("Kamu bisa panggil saya", Nama_Panggilan)
