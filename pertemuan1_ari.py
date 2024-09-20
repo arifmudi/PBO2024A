f9ae8573627ec39fb4733f7cfb04af698704d25e
Envious19 <ari.hendrawan50@gmail.com>
2024-09-20 10:02:52 +0700

    Delete pertemuan1_ari.py

--- pertemuan1_ari.py
+++
@@ -1,8 +0,0 @@
-Nama = "Ari Hendrawan"
-Nama_Panggilan = "Ari atau Envy"
-
-# Nama = input("Masukkan Nama Kamu :")
-# Nama_Panggilan = input("Masukkan Nama Panggilan Kamu :")
-
-print("Nama saya, "+ Nama)
-print("Kamu bisa panggil saya", Nama_Panggilan)
