9dcbd22847b4fa471947f5bc6e48a8aebd7784b1
Envious19 <ari.hendrawan50@gmail.com>
2024-09-20 10:03:30 +0700

    Delete AMARA AZ ZAHRA directory

--- AMARA AZ ZAHRA/pertemuan1_rara.py
+++
@@ -1,9 +0,0 @@
-nama = "Amara Az Zahra"
-nama_panggilan = "Rara"
-
-print("Nama saya, " +nama)
-print("Kamu bisa panggil saya " + nama_panggilan)
-
-
-nama = input ("Masukkan Nama Kamu: ")
-nama_panggilan = input ("Masukkan Nama Panggilan Kamu:")
