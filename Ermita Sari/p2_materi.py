6121efae4e63586601596dfa583b8754e63b752f
unknown <ermitasari790@gmail.com>
2024-09-23 12:52:51 +0700

    upload file

---
+++ Ermita Sari/p2_materi.py
@@ -0,0 +1,13 @@
+class informatika:
+    def __init__ (self , semester, mata_kuliah):
+    self.nama= nama
+    self.semester= semester
+    self.mata_kuliah=mata_kuliah
+    
+
+    def tampilkan_info(self):
+         return f" nama saya adalah {self.nama},saat ini semester{self.semester} 
+        dengan matkul {self.mata_kuliah} direturn matkul keseukaan saya(self.mata_kuliah).")
+
+mahasiswa1=mahasiswa("Ermita Sari","3","PBO")
+Mahasiswa1.tampilkan_info()
