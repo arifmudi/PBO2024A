403612c13b6108f25e32edaff2249997b8f56197
Amara Az Zahra <amaraazzahrah04@gmail.com>
2024-09-27 09:09:23 +0700

    Create tugas2.py
    class

---
+++ AMARA AZ ZAHRA/tugas2.py
@@ -0,0 +1,12 @@
+class informatika:
+  def __init__(self, nama, semester, matkul):
+    self.nama = nama
+    self.semester = semester
+    self.matkul = matkul
+ 
+  def __str__(self):
+    return f"Nama saya adalah {self.nama} saat ini saya semester {self.semester} dengan mata kuliah kesukaan {self.matkul}"
+
+x = informatika("Amaraazzahra", 3, "PBO")
+
+print(x)
