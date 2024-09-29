a4fcd0091bbfa8564e3e0290addc83821e756a99
aldoyuza <155967160+aldoyuza@users.noreply.github.com>
2024-09-29 11:49:53 +0700

    Add files via upload

---
+++ ALDO YUZA ALFAYET/p2_Tugas.py
@@ -0,0 +1,19 @@
+class Informatika:
+    def __init__(self, nama, semester, matakuliah):
+        self.nama = nama
+        self.semester = semester
+        self.matakuliah = matakuliah
+        self.orangKanan = "Ari." 
+
+    def set_orang_kanan(self, orangKanan):
+        self.orangKanan = orangKanan
+
+    def myfunc(self):
+        print(f"Nama saya {self.nama} \nsaat ini semester {self.semester} "
+              f"\ndengan matkul {self.matakuliah}, \nMatkul Favorit {self.matakuliah}. "
+              f"\nOrang Disebelah Kanan {self.orangKanan}.")
+
+# Membuat objek
+variabel = Informatika("Aldo Yuza Alfayet", 3, "PBO")
+variabel.set_orang_kanan("Ari") 
+variabel.myfunc()
