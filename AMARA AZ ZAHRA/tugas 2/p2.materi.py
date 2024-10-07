cfa3b671a3d83ade7f74ac6ecd14644b00e375b7
Amara Az Zahra <amaraazzahrah04@gmail.com>
2024-10-07 08:57:47 +0700

    p2.materi.py

--- AMARA AZ ZAHRA/tugas 2/p2.materi.py
+++ AMARA AZ ZAHRA/tugas 2/p2.materi.py
@@ -6,7 +6,10 @@ class informatika:
 
   def __str__(self):
     return f"Nama saya adalah {self.nama} saat ini saya semester {self.semester} dengan mata kuliah kesukaan {self.matkul}"
+  def teman_sebelah_kanan(self,teman_kanan):
+    return f"Teman di sebelah kanan saya adalah {teman_kanan}."
 
 x = informatika("Amaraazzahra", 3, "PBO")
-
-print(x)
+teman_kanan = "Darell"
+print(x.__str__())
+print(x.teman_sebelah_kanan(teman_kanan))
