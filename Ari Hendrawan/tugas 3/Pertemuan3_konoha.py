ab7ca1a06ea08f4a220fee72063f94689acc77f2
Envious19 <ari.hendrawan50@gmail.com>
2024-10-14 00:33:56 +0700

    Update part 2 Pertemuan3_konoha.py
    revisi untuk ke 4 kalinya pak mohon maaf

--- Ari Hendrawan/tugas 3/Pertemuan3_konoha.py
+++ Ari Hendrawan/tugas 3/Pertemuan3_konoha.py
@@ -1,19 +1,16 @@
 def eligma_encrypt(input_string):
-   
+
     digits = [int(char) for char in input_string if char.isdigit()]
-    if not digits:
-        raise ValueError("Input harus mengandung minimal 1 angka.")
-    
-    jumlah_angka = sum(digits)  
+    jumlah_angka = sum(digits) if digits else 0
     
     hasil_geser = ''.join(
-        chr((ord(char.lower()) - ord('a') + jumlah_angka) % 26 + ord('a')) if char.isalpha() else ''
+        chr((ord(char.lower()) - ord('a') + jumlah_angka) % 26 + ord('a')) if char.islower() else
+        chr((ord(char.upper()) - ord('A') + jumlah_angka) % 26 + ord('A')) if char.isupper() else
+        ''
         for char in input_string
     )
-    
     return hasil_geser
 
 input_string = input("Masukkan pesan Rahasia: ")
-
 kode_string = eligma_encrypt(input_string)  
-print("Kode Rahasia:", kode_string) 
+print("Kode Rahasia:", kode_string)
