7b87760b0dbd6b8e72d57d6f16d8bca893dd60d9
rizkyfaizal2355201020 <rizky01faizal@gmail.com>
2024-10-13 20:00:49 +0700

    Delete the file at 'pertemuan1_rizky.py'

--- pertemuan1_rizky.py
+++
@@ -1,8 +0,0 @@
-nama1 = "Muhammad Rizky"
-panggilan1 = "Muhammad Rizky"
-
-print ("Nama Saya, " + nama1)
-print ("Nickname Saya, " + panggilan1)
-
-nama2 = input ("Nama: ")
-panggilan2 = input ("Panggilan: ")
