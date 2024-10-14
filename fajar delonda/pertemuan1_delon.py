d25dfe2250e1c969de776bf2e728f99077f1e31a
Fajardelonda <delondafajar0@gmail.com>
2024-10-14 14:35:52 +0700

    Delete fajar delonda directory

--- fajar delonda/pertemuan1_delon.py
+++
@@ -1,9 +0,0 @@
-nama = "Fajar Delonda"
-nama_panggilan = "Delon"
-
-print("nama saya, "+nama)
-print("kamu bisa panggil saya", nama_panggilan) 
-
-
-nama = input("masukkan nama kamu: ")
-nama_panggilan = input("masukkan nama panggilan kamu: ")
