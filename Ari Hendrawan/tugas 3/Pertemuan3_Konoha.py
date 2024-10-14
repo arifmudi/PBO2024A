44430a98ec4a78d54e8f3d798328698caa07d6ca
Envious19 <ari.hendrawan50@gmail.com>
2024-10-14 12:19:29 +0700

    Revisi lagi

--- Ari Hendrawan/tugas 3/Pertemuan3_Konoha.py
+++ Ari Hendrawan/tugas 3/Pertemuan3_Konoha.py
@@ -2,6 +2,14 @@ class EligmaEncryptor:
     def __init__(self, masuk_string):
         self.masuk_string = masuk_string
 
+    def _get_valid_input(self):
+        while True:
+            self.masuk_string = input("Masukkan pesan Rahasia: ")
+            if any(char.isdigit() for char in self.masuk_string):
+                break
+            else:
+                print("Bikin apa sih lu? kasih angka kek.")
+
     def _extract_digits(self):
         return [int(char) for char in self.masuk_string if char.isdigit()]
 
@@ -18,21 +26,13 @@ class EligmaEncryptor:
             return ''
 
     def encrypt(self):
+        self._get_valid_input()
         shift = self._calculate_shift()
         hasil_geser = ''.join(
             self._shift_characters(char, shift) for char in self.masuk_string
         )
         return hasil_geser
 
-def get_valid_input():
-    while True:
-        masuk_string = input("Masukkan pesan Rahasia: ")
-        if any(char.isdigit() for char in masuk_string):
-            return masuk_string
-        else:
-            print("Bikin apa sih lu? kasih angka kek.")
-
-masuk_string = get_valid_input()
-pengode = EligmaEncryptor(masuk_string)
+pengode = EligmaEncryptor("")
 kode_string = pengode.encrypt()
 print("Kode Rahasia:", kode_string)
